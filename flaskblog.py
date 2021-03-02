a8571448488f04c6d887b7b15125b48863f282ec
denis <mrisholigare@gmail.com>
2021-03-02 13:44:33 +0300

    dummy database added

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,22 @@
 from flask import Flask ,render_template
 app=Flask(__name__)
 
+posts=[
+    {
+        'author':'denis ligare',
+        'date_posted':'2nd April 2020',
+        'title':'the river and the source',
+        'content':'sssljcsd hgfd jsdhdjsd'
+    },
+    {
+        'author':'moureen Khavere',
+        'date_posted':'2nd December 2020',
+        'title':'Kit mikai',
+        'content':'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum '
+
+    }
+]
+
 @app.route("/")
 def index():
     return render_template("about.html")
