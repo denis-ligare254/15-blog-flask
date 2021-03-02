2edc621e639d1b309f91c2a3f89c82035e8a179a
denis <mrisholigare@gmail.com>
2021-03-02 13:37:09 +0300

    render template added

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,9 @@
-from flask import Flask 
+from flask import Flask ,render_template
 app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hellow world!S"
+    return render_template("about.html")
 
 
 @app.route("/about")
