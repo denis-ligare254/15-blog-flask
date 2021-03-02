d6508f6a73832c9b2fba31b5dfdc5f07c5756947
denis <mrisholigare@gmail.com>
2021-03-02 17:51:59 +0300

    strings added on class regestration form

--- templates/forms.py
+++ templates/forms.py
@@ -6,3 +6,6 @@ from wtforms.validators import DataRequired, Length,Email,EqualTo
 
 class RegestrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
+    Email=StringField('Email',validators=[DataRequired(),Email()])
+    password=StringField('password',validators=[DataRequired()])
+    Confirm_password=StringField('confirm_password',validators=[DataRequired(),EqualTO("password")])
