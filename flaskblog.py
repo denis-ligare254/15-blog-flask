1312767bcc25704990973ad79fd3e50df1ea664f
denis <mrisholigare@gmail.com>
2021-03-02 13:35:14 +0300

    templates added

--- flaskblog.py
+++ flaskblog.py
@@ -3,7 +3,12 @@ app=Flask(__name__)
 
 @app.route("/")
 def index():
-    return "hellow world"
+    return "hellow world!S"
+
+
+@app.route("/about")
+def about():
+    return "hellow world!S here is about"
 
 
 if __name__=="__main__":
